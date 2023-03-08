12c51ad70288937477ac494109975f98f100256b
Carlos Polanco <carlos.polanco0508@gmail.com>
2023-03-08 11:07:38 -0500

    added text to dash

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,14 @@ function App() {
   return (
     <div className="bg-sky-50 md:pb-20 mb-40">
       <Navbar />
-      <div className="w-full  flex  md:pl-20 md:pr-20  md:pt-10 md:pb-20">
+      <div className="w-full  flex  md:pl-20 md:pr-20  md:pt-40 md:pb-20">
         <div
           className="w-full  rounded-md  flex flex-col items-center gap-y-4"
          
         >
           <h1 className="font-bold text-5xl text-center md:text-7xl p-4 ">Welcome to Dash </h1>
           <h1 className="text-xl font-regular text-gray-500 lg:w-1/2 text-center">
-            Visualize your data, get insights, created reports and dashboard.
-            with our team of experts in data visualization
+         Welcome to Dash, the powerful data visualization solution that enables you to easily create interactive and insightful dashboards for your data.
           </h1>
           <div className="w-full lg:w-11/12 max-w-5xl h-64 md:h-96 bg-white rounded-lg md:shadow-2xl">
             <LineChartComponent data={lineData} />
@@ -51,20 +50,12 @@ function App() {
             <PieComponent data={pieData} />
           </div>
           <div className="p-4 space-y-4 flex-1 font-light  max-w-3xl">
-            <h3 className="font-bold text-4xl  ">Lorem Ipsum</h3>
+            <h3 className="font-bold text-4xl  ">Intuitive</h3>
             <p className="leading-9 text-xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptates nostrum mollitia quasi minus ea illo quisquam similique
-              doloribus neque rerum, corrupti magni ut quo? Soluta similique
-              iste vitae hic exercitationem.
+            With our intuitive drag-and-drop interface and extensive library of charts, graphs, and other visualizations, you can create beautiful and informative dashboards in minutes.
             </p>
 
-            <p className="leading-9 text-xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptates nostrum mollitia quasi minus ea illo quisquam similique
-              doloribus neque rerum, corrupti magni ut quo? Soluta similique
-              iste vitae hic exercitationem.
-            </p>
+           
           </div>
         </div>
       </div>
@@ -73,20 +64,12 @@ function App() {
       <div className="flex flex-col lg:flex-row w-full items-center lg:gap-x-8  md:gap-y-8">
          
           <div className="p-4 space-y-4 flex-1 font-light  ">
-            <h3 className="font-bold text-4xl  ">Lorem Ipsum</h3>
+            <h3 className="font-bold text-4xl  ">Real Time</h3>
             <p className="leading-9 text-xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptates nostrum mollitia quasi minus ea illo quisquam similique
-              doloribus neque rerum, corrupti magni ut quo? Soluta similique
-              iste vitae hic exercitationem.
+            Dash also offers powerful features like real-time updates, interactive filtering, and custom styling, giving you complete control over the look and functionality of your dashboard. And with our robust APIs and integrations, you can easily connect your data application to a wide range of data sources and external services.
             </p>
 
-            <p className="leading-9 text-xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptates nostrum mollitia quasi minus ea illo quisquam similique
-              doloribus neque rerum, corrupti magni ut quo? Soluta similique
-              iste vitae hic exercitationem.
-            </p>
+          
           </div>
 
           <div
@@ -109,19 +92,11 @@ function App() {
             <FunnelCompoenent data={funnelData} />
           </div>
           <div className="p-4 space-y-4 flex-1 font-light  max-w-3xl">
-            <h3 className="font-bold text-4xl  ">Lorem Ipsum</h3>
-            <p className="leading-9 text-xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptates nostrum mollitia quasi minus ea illo quisquam similique
-              doloribus neque rerum, corrupti magni ut quo? Soluta similique
-              iste vitae hic exercitationem.
-            </p>
+            <h3 className="font-bold text-4xl  ">Transform </h3>
+          
 
             <p className="leading-9 text-xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptates nostrum mollitia quasi minus ea illo quisquam similique
-              doloribus neque rerum, corrupti magni ut quo? Soluta similique
-              iste vitae hic exercitationem.
+            Easily transform your Excel sheet into a powerful web-based dashboard that you can share with others.
             </p>
           </div>
         </div>
@@ -129,6 +104,7 @@ function App() {
 
       <div className="w-full bg-white flex flex-col items-center justify-center md:p-20  mt-12 space-y-8">
         <h3 className="text-3xl text-center md:text-5xl font-bold">Build your customize dashboard</h3>
+        <p className="leading-9 text-xl text-center w-1/2 font-light">So whether you're looking to explore and analyze your data in new ways, build powerful data-driven applications, or communicate insights to your team and stakeholders, Dash has everything you need to succeed.</p>
         <img src="/images/Main.png" alt={"dashboard"} className="w-full"/>
 
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='bg-white w-full h-14 p-4 flex items-center justify-between '>
-        <p className='font-bold text-xl'>D</p>
+    <div className='bg-white flex items-center justify-center w-full fixed  p-4 border-b'>
+      <div className='w-full flex items-center justify-between   max-w-7xl'>
+        <p className='font-bold text-4xl'>D</p>
         <div className='flex  gap-x-12 h-full text-gray-500'>
             <p>Home</p>
             <p>About</p>
@@ -13,6 +14,7 @@ export default function Navbar() {
         <div className='pt-2 pb-2 pl-4 pr-4 bg-blue-500 rounded-md text-white cursor-pointer'>
             Contact
         </div>
+    </div>
     </div>
   )
 }
